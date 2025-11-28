9b88b9c09e72a6bda340f40b3804d86455078a77
VictoriaRK <vkrushovska@gmail.com>
2025-11-28 11:53:15 +0000

    mini changes and added music

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,45 @@
 module Main exposing (..)
+import Html exposing (text)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.sandbox { init = init, update = update, view = view }
+  
+
+-- MODEL
+
+type alias Score = Int
+
+init : Score
+init =
+  0
+
+
+-- UPDATE
 
 type Msg = Increment | Decrement
 
-update msg model =
+update : Msg -> Score -> Score
+update msg score =
   case msg of
     Increment ->
-      model + 1
+      score + 1
 
-    Decrement ->
-      model - 1
+    Decrement -> (
+      if score == 0 then
+        0 
+      else
+        score - 1)
 
+
+-- VIEW
+
+view : Score -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
+    [ button [ onClick Increment ] [ text "+" ]
     , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Decrement ] [ text "-" ]
     ]
