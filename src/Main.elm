4cb0eee6059ffa01833a0ca270370ef22a6fdc57
VictoriaRK <VictoriaRK@users.noreply.github.com>
2025-11-28 15:01:25 +0000

    deploy: 79ac6d34d11e36638287c5f1322373dcd741b3c9

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,8 @@
 module Main exposing (..)
-import Html exposing (text)
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-
-main =
-  Browser.sandbox { init = init, update = update, view = view }
-  
-
 -- MODEL
 
 type alias Score = Int
@@ -36,10 +31,46 @@ update msg score =
 
 -- VIEW
 
-view : Score -> Html Msg
-view model =
+btn : Score -> Html Msg
+btn model =
   div []
     [ button [ onClick Increment ] [ text "+" ]
     , div [] [ text (String.fromInt model) ]
     , button [ onClick Decrement ] [ text "-" ]
     ]
+
+
+
+
+main = 
+  Browser.sandbox { init = init, update = update, view = view }
+
+view : Score -> Html Msg
+view score =
+    div []
+        [ -- 1. INJECT CSS & FONTS HERE
+          -- Html.node "style" [] [ text ""]
+        
+        -- 2. THE REST IS THE SAME
+        div [ class "page-container" ]
+            [ 
+              -- Navigation Bar
+              div [ id "header-container" ] [ text "My Elm Website" ]
+                , Html.h1 [] [ text "No HTML file needed!" ]
+                , Html.img [ src "media/pictures/me/selfportrait.png", class "responsive-img" ] []
+                ,
+              -- Main Content
+              div [class "main-page"] [
+                -- Left Sidebar
+                div [ class "left-side" ] [ text "Left Nav" ]
+
+                -- Center Column
+              , div [ class "centre" ] [ btn score ]
+
+
+                -- Right Sidebar
+              , div [ class "right-side" ] [ text "Right Nav" ]
+              ]
+            ]
+        ]
+
